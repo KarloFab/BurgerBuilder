7dc0c151f0c414cb3ec3b3a8e8c960b379a45132
KarloFab <fabijanickarlo1@gmail.com>
2019-07-14 19:38:08 +0200

    Added logo css

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import burgerLogo from '../../assets/images/burger-logo.png';
+import styles from './Logo.module.css';
 
 const logo = (props) =>(
-    <div></div>
+    <div className={styles.logo}>
+        <img src={burgerLogo} alt="Burger"/>
+    </div>
 );
 
 export default logo;
