3ffe60d3a0d05db3461a2c07627b55fdfe3cf219
KarloFab <fabijanickarlo1@gmail.com>
2019-08-05 22:18:40 +0200

    Refactored error interceptors

--- src/components/withErrorHandler/withErrorHandler.js
+++ src/components/withErrorHandler/withErrorHandler.js
@@ -1,40 +1,46 @@
-import React, { Fragment, Component } from 'react';
-import Modal from '../../components/UI/Modal/Modal';
+import React, { Fragment, Component } from "react";
+import Modal from "../../components/UI/Modal/Modal";
 
 const withErrorHandler = (WrapperComponent, axios) => {
-    return class extends Component {
+  return class extends Component {
+    state = {
+      error: null
+    };
 
-        state = {
-            error: null
+    componentWillMount() {
+      this.requestInterceptor = axios.interceptors.request.use(req => {
+        this.setState({ error: null });
+        return req;
+      });
+
+      this.responseInterceptor = axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error });
         }
-
-        componentWillMount(){
-            axios.interceptors.request.use(req => {
-                this.setState({ error: null})
-                return req;
-            });
-
-            axios.interceptors.response.use(res=> res, error => {
-                this.setState({ error: error})
-            });
-        }
-
-        errorConfirmedHandler = () => {
-            this.setState({ error: null});
-        }
-
-        render(){
-            return (
-                <Fragment>
-                <Modal show={this.state.error}
-                    clicked={this.errorConfirmedHandler}>
-                    {this.state.error ? this.state.error.message : null}
-                </Modal>
-                <WrapperComponent {...this.props} />
-              </Fragment>
-            )
-        }
+      );
     }
-}
+
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+
+    componentWillUnmount() {
+        axios.interceptors.request.eject(this.requestInterceptor);
+        axios.interceptors.request.eject(this.responseInterceptor);
+    }
+
+    render() {
+      return (
+        <Fragment>
+          <Modal show={this.state.error} clicked={this.errorConfirmedHandler}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrapperComponent {...this.props} />
+        </Fragment>
+      );
+    }
+  };
+};
 
 export default withErrorHandler;
