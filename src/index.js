442f149a7039548713dec6180221c41985bdf6e8
KarloFab <fabijanickarlo1@gmail.com>
2019-11-20 20:55:28 +0100

    Added thunk to redux

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import reducer from "./store/reducers/burgerBuilder";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
 
-const store = createStore(reducer);
+const componseEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, componseEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <Provider store={store}>
     <BrowserRouter>
       <App />
-    </BrowserRouter> 
+    </BrowserRouter>
   </Provider>
 );
 
