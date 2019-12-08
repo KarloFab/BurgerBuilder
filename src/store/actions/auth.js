44db0011b8965eef4c9a085ecea339c6a1616cf9
KarloFab <fabijanickarlo1@gmail.com>
2019-12-08 20:22:35 +0100

    Added sign up method

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,42 +1,51 @@
-
-import * as actionTypes from './actionTypes';
-import axios from 'axios';
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    }
-}
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
 
-export const authSuccess = (authData) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        authData: authData
-    }
-}
+export const authSuccess = authData => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData
+  };
+};
 
-export const authFail = (error) => {
-    return {
-        type: actionTypes.AUTH_FAIL,
-        error: error
-    }
-}
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error
+  };
+};
 
-export const auth = (email, password) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-        axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[AIzaSyDD4x63D-hJvAYjMbVIHo8AWdGAHzME_vE]", authData)
-        .then(response => {
-            console.log(response)
-            dispatch(authSuccess(response.data))
-        })
-        .catch(err => {
-            dispatch(authFail(err))
-        });
+export const auth = (email, password, isSignUp) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDD4x63D-hJvAYjMbVIHo8AWdGAHzME_vE";
+
+    if (!isSignUp) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDD4x63D-hJvAYjMbVIHo8AWdGAHzME_vE";
     }
-}
+
+    axios
+      .post(url, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
