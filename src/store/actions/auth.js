dd6060840484f409d797bc09194da7e62e7787e1
KarloFab <fabijanickarlo1@gmail.com>
2019-12-08 20:38:31 +0100

    Renamed params

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,10 +7,12 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    authData: authData,
+    idToken: token,
+    userId: userId
   };
 };
 
