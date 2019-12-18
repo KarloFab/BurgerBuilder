90c19926623ca4ac96eb3e3abbb3f267c1b19f07
KarloFab <fabijanickarlo1@gmail.com>
2019-12-18 21:12:03 +0100

    Fixed user log out on refresh

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -92,7 +92,7 @@ export const authCheckState = () => {
       } else {
         const  userId = localStorage.getItem("userId")
         dispatch(authSuccess(token, userId));
-        dispatch(checkAuthTimeout(expirationTime.getSecond() - new Date().getSeconds()))
+        dispatch(checkAuthTimeout((expirationTime.getTime() - new Date().getTime()) / 1000))
       }
     }
   }
