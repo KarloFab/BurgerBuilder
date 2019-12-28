06a13e8ae601b7f7109905d156a5b847b239dd8b
KarloFab <fabijanickarlo1@gmail.com>
2019-12-28 13:39:51 +0100

    Expanded auth reducer test

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -8,7 +8,32 @@ describe("auth reducer", () => {
       userId: null,
       error: null,
       loading: false,
-      authRedirect: '/'
+      authRedirect: "/"
+    });
+  });
+
+  it("should store the token upon login", () => {
+    expect(
+      reducer(
+        {
+          token: null,
+          userId: null,
+          error: null,
+          loading: false,
+          authRedirect: "/"
+        },
+        {
+          type: actionTypes.AUTH_SUCCESS,
+          idToken: "some-token",
+          userId: "some-user-id"
+        }
+      )
+    ).toEqual({
+      token: "some-token",
+      userId: "some-user-id",
+      error: null,
+      loading: false,
+      authRedirect: "/"
     });
   });
 });
