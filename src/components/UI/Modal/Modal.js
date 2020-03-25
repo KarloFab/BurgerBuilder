41a8605c636867c31991ef87383119af03705791
KarloFab <fabijanickarlo1@gmail.com>
2020-03-25 17:18:23 +0100

    Refactored Modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,30 +2,36 @@ import React, { Fragment, Component } from "react";
 import styles from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      nextProps.show !== this.props.show ||
-      nextProps.children !== this.props.children
-    );
-  }
+const modal = props => {
+    // shouldComponentUpdate(nextProps, nextState) {
+    //   return (
+    //     nextProps.show !== this.props.show ||
+    //     nextProps.children !== this.props.children
+    //   );
+    // }
 
-  render() {
-    return (
-      <Fragment>
-        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-        <div
-          style={{
-            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
-            opacity: this.props.show ? "1" : "0"
-          }}
-          className={styles.modal}
-        >
-          {this.props.children}
-        </div>
-      </Fragment>
+    return ( <
+        Fragment >
+        <
+        Backdrop show = { props.show }
+        clicked = { props.modalClosed }
+        />{" "} <
+        div style = {
+            {
+                transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+                opacity: props.show ? "1" : "0"
+            }
+        }
+        className = { styles.modal } >
+        { this.props.children } { " " } <
+        /div>{" "} <
+        /Fragment>
     );
-  }
-}
+};
 
-export default Modal;
+export default React.memo(
+    modal,
+    (prevProps, nextProps) =>
+    nextProps.show !== prevProps.show ||
+    nextProps.children !== prevProps.children
+);
